0abc8a40147b8259c996681da2911eb0270453e7
TimH <tmherron09@gmail.com>
2020-07-27 10:24:27 -0500

    New Test AddMethod_AddInt10_CountIsOne

--- CustomListUnitTest/AddTests.cs
+++ CustomListUnitTest/AddTests.cs
@@ -8,13 +8,14 @@ namespace CustomListUnitTest
     [TestClass]
     public class AddTests
     {
+        // Test Add Method Type Int
         [TestMethod]
         public void AddMethod_AddInt10_CountIsOne()
         {
             //  Arrange
             CustomList<int> list = new CustomList<int>();
 
-            int expected;
+            int expected = 1;
             int actual;
 
             //  Act
@@ -23,11 +24,34 @@ namespace CustomListUnitTest
             actual = list.Count;
 
             //  Assert
-
-
-
-
-
+            Assert.AreEqual(expected, actual);
         }
+        
+
+
+
+
+
+
+
+
+
+
+
+        //  Copy Past For Faster Writting
+        //[TestMethod]
+        //public void AddMethod()
+        //{
+        //    //  Arrange
+        //    CustomList<int> list = new CustomList<int>();
+
+        //    int expected;
+        //    int actual;
+
+        //    //  Act
+
+        //    //  Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
