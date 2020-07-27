6ee7058411c35951e407efc10e97328eb2663df1
TimH <tmherron09@gmail.com>
2020-07-27 10:25:40 -0500

    New Unit Test AddMethod_Add1Int6_IndexAt0Is6

--- CustomListUnitTest/AddTests.cs
+++ CustomListUnitTest/AddTests.cs
@@ -47,7 +47,22 @@ namespace CustomListUnitTest
             //  Assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void AddMethod_Add1Int6_IndexAt0Is6()
+        {
+            //  Arrange
+            CustomList<int> list = new CustomList<int>();
 
+            int expected = 6;
+            int actual;
+
+            //  Act
+            list.Add(6);
+            actual = list[0];
+
+            //  Assert
+            Assert.AreEqual(expected, actual);
+        }
 
 
 
