843c30b6a73e9aad47c5e7addeebc0ea291eff2e
TimH <tmherron09@gmail.com>
2020-07-27 15:07:50 -0500

    Add New RemoveMethod Unit Test RemoveMethod_RemoveInt5FromCustomListOf1Through5_CountIs4

--- CustomListUnitTest/RemoveMethodTests.cs
+++ CustomListUnitTest/RemoveMethodTests.cs
@@ -11,6 +11,34 @@ namespace CustomListUnitTest
     [TestClass]
     public class RemoveMethodTests
     {
+        [TestMethod]
+        public void RemoveMethod_RemoveInt5FromCustomListOf1Through5_CountIs4()
+        {
+            //  Arrange
+            CustomList<int>customList = new CustomList<int>();
+            for(int i = 1; i <=5; i++)
+            {
+               customList.Add(i);
+            }
+            int expected = 4;
+            int actual;
+            //  Act
+           customList.Remove(5);
+            actual =customList.Count;
+            //  Assert
+            Assert.AreEqual(expected, actual);
+        }
+       
+
+
+
+
+
+
+
+
+
+
 
 
 
