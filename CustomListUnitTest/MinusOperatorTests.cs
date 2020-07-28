e00530054cd28d34a66eb78328ff7f0bf6a92b2d
TimH <tmherron09@gmail.com>
2020-07-28 09:02:02 -0500

    Add MinusOperator Unit Test Class with one test
    Added new class to test the overload of the - minus operator.
    Added new test for basic subtraction, i.e. CustomList of 1,3,5 minus CustomList of 2,1,6 should equal CustomList of 3,5.
    Where any like elements are subtracted from first.

---
+++ CustomListUnitTest/MinusOperatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomListClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    class MinusOperatorTests
+    {
+
+        [TestMethod]
+        public void MinusOperator_CustomList135Minus216_CustomList35()
+        {
+            //  Arrange
+            CustomList<int> minuend = new CustomList<int>();
+            minuend.Add(1);
+            minuend.Add(3);
+            minuend.Add(5);
+            
+            CustomList<int> subtrahend = new CustomList<int>();
+            subtrahend.Add(2);
+            subtrahend.Add(1);
+            subtrahend.Add(6);
+
+            CustomList<int> expected = new CustomList<int>();
+            expected.Add(3);
+            expected.Add(5);
+            CustomList<int> actual;
+
+            //  Act
+            actual = minuend - subtrahend;
+
+
+            //  Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+
+    }
+}
