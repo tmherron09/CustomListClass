d1c1c55d94b59d020e9fc377fb3794fa6a6d58c6
TimH <tmherron09@gmail.com>
2020-07-27 21:06:03 -0500

    Add new Refactored PlusOperator test to account for no Equals() overload
    AddCustomList135ToCustomList246 to accomodate the two objects were not equal based object.Equals()

--- CustomListUnitTest/PlusOperatorOverloadTests.cs
+++ CustomListUnitTest/PlusOperatorOverloadTests.cs
@@ -26,6 +26,51 @@ namespace CustomListUnitTest
             even.Add(4);
             even.Add(6);
 
+            CustomList<int> expectedSum = new CustomList<int>();
+            expectedSum.Add(1);
+            expectedSum.Add(3);
+            expectedSum.Add(5);
+            expectedSum.Add(2);
+            expectedSum.Add(4);
+            expectedSum.Add(6);
+            bool hasPassed = true;
+
+            CustomList<int> actualSum;
+
+            // Act
+            actualSum = odd + even;
+
+            if (expectedSum.Count != actualSum.Count)
+            {
+                hasPassed = false;
+            }
+
+            for (int i = 0; i < expectedSum.Count; i++)
+            {
+                if (expectedSum[i] != actualSum[i])
+                {
+                    hasPassed = false;
+                }
+            }
+
+
+            // Assert
+            Assert.IsTrue(hasPassed);
+        }
+        [TestMethod]
+        public void PlusOperator_AddCustomList135ToCustomList246_EqualsCustomListOf135246()
+        {
+            //  Arrange
+            CustomList<int> odd = new CustomList<int>();
+            odd.Add(1);
+            odd.Add(3);
+            odd.Add(5);
+
+            CustomList<int> even = new CustomList<int>();
+            even.Add(2);
+            even.Add(4);
+            even.Add(6);
+
             CustomList<int> expected = new CustomList<int>();
             expected.Add(1);
             expected.Add(3);
@@ -37,12 +82,11 @@ namespace CustomListUnitTest
 
             // Act
             actual = odd + even;
-
+            
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
-
 
     }
 }
